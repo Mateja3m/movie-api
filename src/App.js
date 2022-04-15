dbf3bc6fb021d72a9e6d7cfada809d44eed01334
Milan <milanmatejicdev@gmail.comn>
2022-04-15 14:47:05 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
 import { useState, useEffect, createContext } from "react";
-// import Movies from "./Movies";
-// import Nav from "./Nav";
+import Nav from "./Nav";
 import "bootstrap/dist/css/bootstrap.min.css";
-import * as ReactBootStrap from 'react-bootstrap'
+import * as ReactBootStrap from "react-bootstrap";
 
 import { Container, Button } from "react-bootstrap";
 
@@ -11,6 +10,8 @@ export const MovieContext = createContext();
 
 function App() {
   const [movie, setMovie] = useState([]);
+  const [show, setShow] = useState('');
+
 
   const getMovie = async () => {
     const response = await fetch(
@@ -34,37 +35,53 @@ function App() {
 
   useEffect(() => {
     getMovie();
+    setShow(false)
   }, []);
-  return (
-    <MovieContext.Provider value={[movie]}>
-      <Container>
-        <ReactBootStrap.Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Year</th>
-              <th>Title</th>
-              <th>Rating</th>
-              <th>Release</th>
-            </tr>
-          </thead>
-          <tbody>
-            {movie &&
-              movie.map((item) => (
-                <tr key={item._id}>
-                  <td>{item.year}</td>
-                  <td>{item.title}</td>
-                  <td>{item.rating}</td>
-                  <td>{item.release}</td>
-                </tr>
-              ))}
-          </tbody>
-        </ReactBootStrap.Table>
-        <Button variant="primary" size="lg" onClick={getMovie}>
-          Click me
-        </Button>
-      </Container>
-    </MovieContext.Provider>
-  );
+
+  if (show === true) {
+    return (
+      <MovieContext.Provider>
+        <Container>
+          <Nav />
+          <Button variant="primary" size="lg" onClick={() => setShow(!show)}>
+            Click me
+          </Button>
+        </Container>
+      </MovieContext.Provider>
+    );
+  } else {
+    return (
+      <MovieContext.Provider value={[movie]}>
+        <Container>
+          <ReactBootStrap.Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Year</th>
+                <th>Title</th>
+                <th>Rating</th>
+                <th>Release</th>
+              </tr>
+            </thead>
+            <tbody>
+              {movie &&
+                movie.map((item) => (
+                  <tr key={item._id}>
+                    <td>{item.year}</td>
+                    <td>{item.title}</td>
+                    <td>{item.rating}</td>
+                    <td>{item.release}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </ReactBootStrap.Table>
+          <Nav />
+          <Button variant="primary" size="lg" onClick={() => setShow(!show)}>
+            Click me
+          </Button>
+        </Container>
+      </MovieContext.Provider>
+    );
+  }
 }
 
 export default App;
